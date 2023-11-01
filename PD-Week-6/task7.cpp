0b3b53a425040bec1c4550d7198c92bf66d6849a
Malik Bilal <bilalyaseen2k4@gmail.com>
2023-11-01 23:57:24 +0500

    Update task7.cpp

--- PD-Week-6/task7.cpp
+++ PD-Week-6/task7.cpp
@@ -1,69 +1,60 @@
 #include <iostream>
 using namespace std;
 
-string calculateHotelPrices(string month, int numberOfStays);
+string checkStudentStatus(int examHour, int examMinute, int studentHour, int studentMinute);
 
-int main()
+main()
 {
-    string month;
-    int numberOfStays;
-    cout << "Enter the month (May, June, July, August, September, October): ";
-    cin >> month;
-    cout << "Enter the number of stays: ";
-    cin >> numberOfStays;
-    string result = calculateHotelPrices(month, numberOfStays);
-    cout << result;
+    int examHour, examMinute, studentMinute, studentHour;
+    cout << "Enter Exam Starting Time (hour): ";
+    cin >> examHour;
+    cout << "Enter Exam Starting Time (minutes): ";
+    cin >> examMinute;
+    cout << "Enter student hour of arrival: ";
+    cin >> studentHour;
+    cout << "Enter student hour of arrival: ";
+    cin >> studentMinute;
+    cout << checkStudentStatus(examHour, examMinute, studentHour, studentMinute);
 }
 
-string calculateHotelPrices(string month, int numberOfStays)
+string checkStudentStatus(int examHour, int examMinute, int studentHour, int studentMinute)
 {
-    float priceStudio, priceApartment;
-    string result;
+    int totalExamMinutes, totalStudentMinutes, timeDifference, timeInHours, timeInMinutes;
+    totalExamMinutes = (examHour * 60) + examMinute;
+    totalStudentMinutes = (studentHour * 60) + studentMinute;
+    if (totalExamMinutes == totalStudentMinutes)
+    {
+        return "On time";
+    }
+    else if (totalExamMinutes > totalStudentMinutes)
+    {
 
-    if (month == "May" || month == "October")
-    {
-        if (numberOfStays <= 7)
+        if (totalStudentMinutes < 60)
         {
-            priceStudio = 50 * numberOfStays;
-            priceApartment = 65 * numberOfStays;
-        }
-        else if (numberOfStays <= 14)
-        {
-            priceStudio = 50 * numberOfStays - 0.05 * 50 * numberOfStays;
-            priceApartment = 65 * numberOfStays;
-        }
-        else
-        {
-            priceStudio = 50 * numberOfStays - 0.3 * 50 * numberOfStays;
-            priceApartment = 65 * numberOfStays - 0.1 * 65 * numberOfStays;
-        }
-    }
-    else if (month == "June" || month == "September")
-    {
-        if (numberOfStays <= 14)
-        {
-            priceStudio = 75.20 * numberOfStays;
-            priceApartment = 68.70 * numberOfStays;
-        }
-        else
-        {
-            priceStudio = 75.20 * numberOfStays - 0.2 * 75.20 * numberOfStays;
-            priceApartment = 68.70 * numberOfStays - 0.1 * 68.70 * numberOfStays;
-        }
-    }
-    else if (month == "July" || month == "August")
-    {
-        priceStudio = 76 * numberOfStays;
-        if (numberOfStays > 14)
-        {
-            priceApartment = 77 * numberOfStays - 0.1 * 77 * numberOfStays;
-        }
-        else
-        {
-            priceApartment = 77 * numberOfStays;
-        }
-    }
 
-    result = "Apartment: $" + to_string(priceApartment) + "\nStudio: $" + to_string(priceStudio);
-    return result;
+            return "Early \n" + to_string(totalStudentMinutes) + " minutes before the start";
+        }
+        else
+        {
+            timeDifference = totalExamMinutes - totalStudentMinutes;
+            timeInHours = timeDifference / 60;
+            timeInMinutes = timeDifference % 60;
+            return "Early \n" + to_string(timeInHours) + ":" + to_string(timeInMinutes) + " hours before the start";
+        }
+    }
+    else if (totalExamMinutes < totalStudentMinutes)
+    {
+        if (totalStudentMinutes < 60)
+        {
+
+            return "Late \n" + to_string(totalStudentMinutes) + " minutes after the start";
+        }
+        else
+        {
+            timeDifference = totalStudentMinutes - totalExamMinutes;
+            timeInHours = timeDifference / 60;
+            timeInMinutes = timeDifference % 60;
+            return "Late \n" + to_string(timeInHours) + ":" + to_string(timeInMinutes) + " hours after the start";
+        }
+    }
 }
